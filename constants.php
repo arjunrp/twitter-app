0ecaeb4a38bc98bdad25ff1f0ee8559b33da8e72
Arjun RP <arj.arjun@gmail.com>
2015-04-11 10:48:17 +0530

    Constants including keys and db credentials

---
+++ constants.php
@@ -0,0 +1,23 @@
+<?php
+/*
+	All Constant Values
+*/
+
+
+define('TWITTER_KEY','');
+define('TWITTER_SECRET','');
+
+
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+define('DB_DATABASE','billing');
+define('DB_SERVER','localhost');
+
+
+
+/*
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+define('DB_DATABASE','billing');
+define('DB_SERVER','localhost');
+*/
