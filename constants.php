8c7083f78c59083b7d7651046b3ea27686b26abf
Arjun RP <arj.arjun@gmail.com>
2015-04-12 20:57:45 +0530

    Adding constants

---
+++ constants.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	All Constant Values
+*/
+
+
+define('TWITTER_KEY','');
+define('TWITTER_SECRET','');
+
+define('TWITTER_ACCESS_TOKEN','');
+define('TWITTER_ACCESS_TOKEN_SECRET','');
+
+
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+define('DB_DATABASE','billing');
+define('DB_SERVER','localhost');
+
+
